f05c48e93f0d8e4a9c89fc03ae9f622a345c2dbe
sbin0819 <sbinha123@gmail.com>
2023-05-14 10:32:02 +0900

    chore: working mdx

--- next.config.js
+++ next.config.js
@@ -4,7 +4,16 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true,
+    mdxRs: true,
   },
 };
 
-module.exports = nextConfig;
+const withMDX = require('@next/mdx')({
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+module.exports = withMDX(nextConfig);
