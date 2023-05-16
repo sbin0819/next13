a11724a8cbaa15379919cd65fa7f6838b34ea887
sbin0819 <sbinha123@gmail.com>
2023-05-17 05:02:40 +0900

    feat: edit mdx

--- app/posts/[slug]/MDX.tsx
+++ app/posts/[slug]/MDX.tsx
@@ -1,11 +1,33 @@
 'use client';
 
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote';
+import Image from 'next/image';
+// import { Heading, InlineCode, Pre, Table, Text } from 'my-components'
+
+const ResponsiveImage = (props: any) => (
+  <Image
+    alt={props.alt}
+    sizes="100vw"
+    style={{ width: '100%', height: 'auto' }}
+    {...props}
+  />
+);
+
+const components = {
+  h1: (props: any) => <h1 {...props} className="text-2xl font-bold" />,
+  h2: (props: any) => <h2 className="text-xl font-bold" />,
+  p: (props: any) => (
+    <p className="text-gray-500 dark:text-gray-400" {...props} />
+  ),
+  pre: (props: any) => <div {...props} />,
+  code: (props: any) => <code {...props} />,
+  img: ResponsiveImage,
+};
 
 interface MDXContentProps {
   source: MDXRemoteProps;
 }
 
 export default function MDXContent({ source }: MDXContentProps) {
-  return <MDXRemote {...source} />;
+  return <MDXRemote {...source} components={components} />;
 }
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,9 +1,21 @@
-'use client';
-import About from '@/data/_posts/about.mdx';
-export default function Page() {
+import { getPosts } from '@/utils/mdxUtils';
+
+async function getData() {
+  return await getPosts();
+}
+
+export default async function Page() {
+  const data = await getData();
   return (
-    <div>
-      <About />
+    <div className="mx-auto w-full">
+      {data.map((post: any, idx) => (
+        <div
+          key={idx}
+          className="my-2 flex max-w-xl items-center justify-center bg-blue-400"
+        >
+          {post.title}
+        </div>
+      ))}
     </div>
   );
 }
