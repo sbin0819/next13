97381c23b03a11dd1c788840c08f4c034220e5ef
sbin <sbinha123@gmail.com>
2023-06-07 13:40:55 +0900

    feat: set storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: 'next/core-web-vitals',
+  extends: ['next/core-web-vitals', 'plugin:storybook/recommended'],
+  rules: {
+    'react/no-unescaped-entities': 0,
+  },
 };
