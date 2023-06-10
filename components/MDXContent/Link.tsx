e9e828d5f0174e38f7b573be83a387e00edb3576
sbin <sbinha123@gmail.com>
2023-06-10 17:57:41 +0900

    fix: fix CustomLink

--- components/MDXContent/Link.tsx
+++ components/MDXContent/Link.tsx
@@ -7,23 +7,8 @@ interface CustomLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
 }
 
 const CustomLink = ({ href, ...rest }: CustomLinkProps) => {
-  const isInternalLink = href && href.startsWith('/');
-  const isAnchorLink = href && href.startsWith('#');
-
-  if (isInternalLink) {
-    return (
-      <Link href={href}>
-        <a {...rest} />
-      </Link>
-    );
-  }
-
-  if (isAnchorLink) {
-    return <a href={href} {...rest} />;
-  }
-
   return (
-    <a
+    <Link
       className="special-underline-new no-underline hover:text-gray-100 dark:hover:text-gray-100"
       target="_blank"
       rel="noopener noreferrer"
